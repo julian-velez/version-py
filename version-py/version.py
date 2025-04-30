79001a666eb3e4aa528e20f362b3d2d98474aec5
julian-velez <60607869+julian-velez@users.noreply.github.com>
2025-04-29 20:05:11 -0500

    version-py
    version-py

---
+++ version-py/version.py
@@ -0,0 +1,6 @@
+# Importamos el módulo 'sys', que proporciona acceso a variables y funciones del sistema
+import sys 
+
+# Imprimimos la versión de Python que está utilizando actualmente el intérprete
+# 'sys.version' es una cadena de texto que contiene información sobre la versión
+print(sys.version)
